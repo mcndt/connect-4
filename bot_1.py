64c37e32364a00d5dce4a67aae2b774a1c5e205a
mcndt <maximiocndt@gmail.com>
2017-11-23 00:12:40 +0100

    removed numpy jumbo

--- bot_1.py
+++ bot_1.py
@@ -6,22 +6,3 @@ def legal_moves(board):
 
 def generate_move(board, player, saved_state):
     return legal_moves(board)[random.randint(0, len(legal_moves(board))-1)]
-
-import os
-
-cwd = os.getcwd()  # Get the current working directory (cwd)
-files = os.listdir(cwd)  # Get all the files in that directory
-print("Files in '%s': %s" % (cwd, files))
-
-y = np.load('dump9.npy')
-print(y)
-
-p1 = np.where(y == 1, True, False)
-p2 = np.where(y == 2, True, False)
-
-B = np.zeros((6, 7, 2))
-B[:, :, 0] = p1
-B[:, :, 1] = p2
-
-print(B[:, :, 0])
-print(B[:, :, 1])
