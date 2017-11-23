334e347f531a28d4082e820381d4077144a808d6
felixguo97 <felixguo97@gmail.com>
2017-11-23 17:51:49 -0500

    Added "If first move of game, return center column" procedure.

--- bot_1.py
+++ bot_1.py
@@ -5,4 +5,7 @@ def legal_moves(board):
     return np.arange(board.shape[1])[np.sum(board == 0, axis=0) > 0]
 
 def generate_move(board, player, saved_state):
-    return legal_moves(board)[random.randint(0, len(legal_moves(board))-1)]
+    if sum(board) == 0:
+        return 4 ## if board is empty (all 0), return center column
+    else:
+        legal_moves(board)[random.randint(0, len(legal_moves(board))-1)]
